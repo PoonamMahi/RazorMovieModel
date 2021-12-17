507c637de3dba8cc57671d1c8ca726f02e91056e
Poonam <poonammahi379@gmail.com>
2021-12-17 02:01:52 -0500

    first

---
+++ RazorPagesMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RazorPagesMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
